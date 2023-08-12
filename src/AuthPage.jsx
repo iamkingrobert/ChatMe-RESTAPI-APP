3e10b50c37899d424a0bab5e362cb19334ae5d35
King Robert Kehinde <73795109+iamkingrobert@users.noreply.github.com>
2023-08-12 12:47:59 +0100

    Connected Backend

--- src/AuthPage.jsx
+++ src/AuthPage.jsx
@@ -6,7 +6,7 @@ const AuthPage = (props) => {
     e.preventDefault();
     const { value } = e.target[0];
     axios
-      .post("https://chat-me-backend.vercel.app/authenticate", { username: value })
+      .post("https://chatme-backend.onrender.com/authenticate", { username: value })
       .then((r) => props.onAuth({ ...r.data, secret: value }))
       .catch((e) => console.log("error", e));
   };
